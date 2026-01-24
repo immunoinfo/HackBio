0f77613c2cfd53ee36c02a0e283dde1643334795
immunoinfo <sanganihemi@gmail.com>
2026-01-24 09:40:22 +0530

    Add files via upload

---
+++ simple_code.R
@@ -0,0 +1,6 @@
+person <- 'Hemi'
+affiliation <- 'Rutgers University'
+fav_gene <- 'CFH'
+organism <- 'Homa sapiens'
+introduction <- paste0('Hi, my name is ', person, '. I got my Master\'s degree from ', affiliation, '. I am an Immunologist so my favourite gene is ', fav_gene, ' from ', organism, '.')
+print(introduction)
